8618063c6b89b9ca1e500b5406ddede9d1fc25c6
Zeswen <jlopez@wordlist.com>
2021-03-15 17:11:15 +0100

    Revert "Sound null safety for transformer_page_view"
    This reverts commit c603ec4dea1adb69844d10fa9e31853fdf8d7fc6.

--- lib/index_controller.dart
+++ lib/index_controller.dart
@@ -8,14 +8,14 @@ class IndexController extends ChangeNotifier {
   static const int PREVIOUS = -1;
   static const int MOVE = 0;
 
-  late Completer _completer;
+  Completer _completer;
 
-  int? index;
-  late bool animation;
-  int? event;
+  int index;
+  bool animation;
+  int event;
 
   Future move(int index, {bool animation: true}) {
-    this.animation = animation;
+    this.animation = animation ?? true;
     this.index = index;
     this.event = MOVE;
     _completer = new Completer();
@@ -25,7 +25,7 @@ class IndexController extends ChangeNotifier {
 
   Future next({bool animation: true}) {
     this.event = NEXT;
-    this.animation = animation;
+    this.animation = animation ?? true;
     _completer = new Completer();
     notifyListeners();
     return _completer.future;
@@ -33,7 +33,7 @@ class IndexController extends ChangeNotifier {
 
   Future previous({bool animation: true}) {
     this.event = PREVIOUS;
-    this.animation = animation;
+    this.animation = animation ?? true;
     _completer = new Completer();
     notifyListeners();
     return _completer.future;
